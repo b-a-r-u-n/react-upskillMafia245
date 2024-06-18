6a41a4604811b93b11c8672604617163ef04978b
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-06-18 20:13:17 +0530

    Completed

--- ReactCustomHook/src/App.jsx
+++ ReactCustomHook/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import useFetch from './Hooks/useFetch';
+import {CardFrame,Error,Loading} from './Components';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [error , setError] = useState(false);
+  const [loading , setLoading] = useState(true);
+
+  const fetch = useFetch(setError,setLoading);
+  console.log(fetch);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-black bg-fixed text-white'>
+      {
+        error ? (loading ? <Loading /> : <Error />) : (loading ? <Loading /> : <CardFrame fetch={fetch}/>)
+      }
+    </div>
   )
 }
 
